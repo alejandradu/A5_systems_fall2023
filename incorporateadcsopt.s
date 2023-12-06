0bae550105391c90a203cd29d8b4a7d974c40f11
Siling Song <ss4799@princeton.edu>
2023-12-05 21:22:31 -0500

    experimenting with not simplified addings

--- incorporateadcsopt.s
+++ incorporateadcsopt.s
@@ -176,7 +176,7 @@ BigInt_add:
     lsl x1, x1, 3
     add x0, x0, x1
     ldr x0, [x0]
-    adds ULSUM, ULSUM, x0 // now c flag has the information of overflow
+    adcs ULSUM, ULSUM, x0 // now c flag has the information of overflow
     // just checking what the c flag is rn
     bcc checkifnocarry4
     mov ULCARRY, 0
