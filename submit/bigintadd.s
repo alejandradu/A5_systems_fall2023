a3348c1956849842db60422d23af8e89f63d029e
Alejandra Duran <ad2002@princeton.edu>
2023-12-11 14:12:42 -0500

    right bigintadd.s

--- submit/bigintadd.s
+++ submit/bigintadd.s
@@ -1,244 +1,247 @@
-	.arch armv8-a
-	.file	"bigintadd.c"
-	.text
-	.align	2
-	.type	BigInt_larger, %function
+//--------------------------------------------------------------------
+// bigintadd.s                                                            
+// Author: Siling & Alejandra                                         
+//--------------------------------------------------------------------
+.equ TRUE, 1
+.equ FALSE, 0
+.equ AULDIGITS, 8
+.equ MAX_DIGITS, 32768
+//--------------------------------------------------------------------
+    .section .rodata
+//--------------------------------------------------------------------
+    .section .data
+//--------------------------------------------------------------------
+    .section .bss
+//--------------------------------------------------------------------
+    .section .text
+    //----------------------------------------------------------------
+    // Return the larger of lLength1 and lLength2.
+    // static long BigInt_larger(long lLength1, long lLength2)
+    //----------------------------------------------------------------
+    // Must be a multiple of 16
+    .equ BIG_INT_LARGER_ST, 32
+    // Local Variable Stack Offsets:
+    .equ LLARGER, 8
+    // Parameter stack Offsets:
+    .equ LLENGTH2, 16
+    .equ LLENGTH1, 24
 BigInt_larger:
-.LFB0:
-	.cfi_startproc
-	sub	sp, sp, #32
-	.cfi_def_cfa_offset 32
-	str	x0, [sp, 8]
-	str	x1, [sp]
-	ldr	x1, [sp, 8]
-	ldr	x0, [sp]
-	cmp	x1, x0
-	ble	.L2
-	ldr	x0, [sp, 8]
-	str	x0, [sp, 24]
-	b	.L3
-.L2:
-	ldr	x0, [sp]
-	str	x0, [sp, 24]
-.L3:
-	ldr	x0, [sp, 24]
-	add	sp, sp, 32
-	.cfi_def_cfa_offset 0
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	BigInt_larger, .-BigInt_larger
-	.section	.rodata
-	.align	3
-.LC0:
-	.string	"bigintadd.c"
-	.align	3
-.LC1:
-	.string	"oAddend1 != NULL"
-	.align	3
-.LC2:
-	.string	"oAddend2 != NULL"
-	.align	3
-.LC3:
-	.string	"oSum != NULL"
-	.align	3
-.LC4:
-	.string	"oSum != oAddend1"
-	.align	3
-.LC5:
-	.string	"oSum != oAddend2"
-	.text
-	.align	2
-	.global	BigInt_add
-	.type	BigInt_add, %function
+    // Prolog
+    // push the entire frame
+    sub sp, sp, BIG_INT_LARGER_ST
+    // store the return address given by caller saved in x30
+    str x30, [sp]
+    // store the first parameter given by caller saved in x0
+    str x0, [sp, LLENGTH1]
+    // store the second parameter given by caller saved in x1
+    str x1, [sp, LLENGTH2]
+
+
+    // long lLarger;
+    ldr x0, [sp, LLENGTH1]
+    ldr x1, [sp, LLENGTH2]
+    cmp x0, x1
+    ble else1
+    // lLarger = lLength1;
+    str x0, [sp, LLARGER]
+    // goto endif1;
+    b endif1
+    else1:
+    // lLarger = lLength2;
+    str x1, [sp, LLARGER]
+    endif1:
+    // Epilog and return lLarger
+        // the callee should save the return value in x0
+        ldr     x0, [sp, LLARGER]
+        // ret branches to address x30
+        ldr     x30, [sp]
+        // pop the stack frame:
+        add     sp, sp, BIG_INT_LARGER_ST
+        ret
+        .size   BigInt_larger, (. - BigInt_larger)
+    //--------------------------------------------------------------
+    // Assign the sum of oAddend1 and oAddend2 to oSum.  oSum should
+    // be distinct from oAddend1 and oAddend2.  Return 0 (FALSE) if 
+    // an overflow occurred, and 1 (TRUE) otherwise. 
+    // int BigInt_add (BigInt_T oAddend1, BigInt_T oAddend2, 
+    // BigInt_T oSum)
+    //--------------------------------------------------------------
+    // must be a multiple of 16
+    .equ BIGINT_ADD_STACK_BYTECOUNT, 64
+    // local variable stack offsets:
+    .equ ULCARRY, 8
+    .equ ULSUM, 16
+    .equ LINDEX, 24
+    .equ LSUMLENGTH, 32
+    // parameter stack offsets (reads backwards):
+    .equ OSUM, 40
+    .equ OADDEND2, 48
+    .equ OADDEND1, 56
+    .global BigInt_add
 BigInt_add:
-.LFB1:
-	.cfi_startproc
-	stp	x29, x30, [sp, -80]!
-	.cfi_def_cfa_offset 80
-	.cfi_offset 29, -80
-	.cfi_offset 30, -72
-	mov	x29, sp
-	str	x0, [sp, 40]
-	str	x1, [sp, 32]
-	str	x2, [sp, 24]
-	ldr	x0, [sp, 40]
-	cmp	x0, 0
-	bne	.L6
-	adrp	x0, __PRETTY_FUNCTION__.0
-	add	x3, x0, :lo12:__PRETTY_FUNCTION__.0
-	mov	w2, 41
-	adrp	x0, .LC0
-	add	x1, x0, :lo12:.LC0
-	adrp	x0, .LC1
-	add	x0, x0, :lo12:.LC1
-	bl	__assert_fail
-.L6:
-	ldr	x0, [sp, 32]
-	cmp	x0, 0
-	bne	.L7
-	adrp	x0, __PRETTY_FUNCTION__.0
-	add	x3, x0, :lo12:__PRETTY_FUNCTION__.0
-	mov	w2, 42
-	adrp	x0, .LC0
-	add	x1, x0, :lo12:.LC0
-	adrp	x0, .LC2
-	add	x0, x0, :lo12:.LC2
-	bl	__assert_fail
-.L7:
-	ldr	x0, [sp, 24]
-	cmp	x0, 0
-	bne	.L8
-	adrp	x0, __PRETTY_FUNCTION__.0
-	add	x3, x0, :lo12:__PRETTY_FUNCTION__.0
-	mov	w2, 43
-	adrp	x0, .LC0
-	add	x1, x0, :lo12:.LC0
-	adrp	x0, .LC3
-	add	x0, x0, :lo12:.LC3
-	bl	__assert_fail
-.L8:
-	ldr	x1, [sp, 24]
-	ldr	x0, [sp, 40]
-	cmp	x1, x0
-	bne	.L9
-	adrp	x0, __PRETTY_FUNCTION__.0
-	add	x3, x0, :lo12:__PRETTY_FUNCTION__.0
-	mov	w2, 44
-	adrp	x0, .LC0
-	add	x1, x0, :lo12:.LC0
-	adrp	x0, .LC4
-	add	x0, x0, :lo12:.LC4
-	bl	__assert_fail
-.L9:
-	ldr	x1, [sp, 24]
-	ldr	x0, [sp, 32]
-	cmp	x1, x0
-	bne	.L10
-	adrp	x0, __PRETTY_FUNCTION__.0
-	add	x3, x0, :lo12:__PRETTY_FUNCTION__.0
-	mov	w2, 45
-	adrp	x0, .LC0
-	add	x1, x0, :lo12:.LC0
-	adrp	x0, .LC5
-	add	x0, x0, :lo12:.LC5
-	bl	__assert_fail
-.L10:
-	ldr	x0, [sp, 40]
-	ldr	x2, [x0]
-	ldr	x0, [sp, 32]
-	ldr	x0, [x0]
-	mov	x1, x0
-	mov	x0, x2
-	bl	BigInt_larger
-	str	x0, [sp, 56]
-	ldr	x0, [sp, 24]
-	ldr	x0, [x0]
-	ldr	x1, [sp, 56]
-	cmp	x1, x0
-	bge	.L11
-	ldr	x0, [sp, 24]
-	add	x0, x0, 8
-	mov	x2, 262144
-	mov	w1, 0
-	bl	memset
-.L11:
-	str	xzr, [sp, 72]
-	str	xzr, [sp, 64]
-	b	.L12
-.L15:
-	ldr	x0, [sp, 72]
-	str	x0, [sp, 48]
-	str	xzr, [sp, 72]
-	ldr	x1, [sp, 40]
-	ldr	x0, [sp, 64]
-	lsl	x0, x0, 3
-	add	x0, x1, x0
-	ldr	x0, [x0, 8]
-	ldr	x1, [sp, 48]
-	add	x0, x1, x0
-	str	x0, [sp, 48]
-	ldr	x1, [sp, 40]
-	ldr	x0, [sp, 64]
-	lsl	x0, x0, 3
-	add	x0, x1, x0
-	ldr	x0, [x0, 8]
-	ldr	x1, [sp, 48]
-	cmp	x1, x0
-	bcs	.L13
-	mov	x0, 1
-	str	x0, [sp, 72]
-.L13:
-	ldr	x1, [sp, 32]
-	ldr	x0, [sp, 64]
-	lsl	x0, x0, 3
-	add	x0, x1, x0
-	ldr	x0, [x0, 8]
-	ldr	x1, [sp, 48]
-	add	x0, x1, x0
-	str	x0, [sp, 48]
-	ldr	x1, [sp, 32]
-	ldr	x0, [sp, 64]
-	lsl	x0, x0, 3
-	add	x0, x1, x0
-	ldr	x0, [x0, 8]
-	ldr	x1, [sp, 48]
-	cmp	x1, x0
-	bcs	.L14
-	mov	x0, 1
-	str	x0, [sp, 72]
-.L14:
-	ldr	x1, [sp, 24]
-	ldr	x0, [sp, 64]
-	lsl	x0, x0, 3
-	add	x0, x1, x0
-	ldr	x1, [sp, 48]
-	str	x1, [x0, 8]
-	ldr	x0, [sp, 64]
-	add	x0, x0, 1
-	str	x0, [sp, 64]
-.L12:
-	ldr	x1, [sp, 64]
-	ldr	x0, [sp, 56]
-	cmp	x1, x0
-	blt	.L15
-	ldr	x0, [sp, 72]
-	cmp	x0, 1
-	bne	.L16
-	ldr	x0, [sp, 56]
-	cmp	x0, 32768
-	bne	.L17
-	mov	w0, 0
-	b	.L18
-.L17:
-	ldr	x1, [sp, 24]
-	ldr	x0, [sp, 56]
-	lsl	x0, x0, 3
-	add	x0, x1, x0
-	mov	x1, 1
-	str	x1, [x0, 8]
-	ldr	x0, [sp, 56]
-	add	x0, x0, 1
-	str	x0, [sp, 56]
-.L16:
-	ldr	x0, [sp, 24]
-	ldr	x1, [sp, 56]
-	str	x1, [x0]
-	mov	w0, 1
-.L18:
-	ldp	x29, x30, [sp], 80
-	.cfi_restore 30
-	.cfi_restore 29
-	.cfi_def_cfa_offset 0
-	ret
-	.cfi_endproc
-.LFE1:
-	.size	BigInt_add, .-BigInt_add
-	.section	.rodata
-	.align	3
-	.type	__PRETTY_FUNCTION__.0, %object
-	.size	__PRETTY_FUNCTION__.0, 11
-__PRETTY_FUNCTION__.0:
-	.string	"BigInt_add"
-	.ident	"GCC: (GNU) 11.3.1 20221121 (Red Hat 11.3.1-4)"
-	.section	.note.GNU-stack,"",@progbits
+    // Prolog
+    sub  sp, sp, BIGINT_ADD_STACK_BYTECOUNT
+    str x30, [sp]
+    str x0, [sp, OADDEND1]
+    str x1, [sp, OADDEND2]
+    str x2, [sp, OSUM]
+    // unsigned long ulCarry;
+    // unsigned long ulSum;
+    // long lIndex;
+    // long lSumLength;
+    // lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength);
+    ldr x0, [sp,OADDEND1]
+    ldr x0, [x0]
+    ldr x1, [sp, OADDEND2]
+    ldr x1, [x1]
+    bl  BigInt_larger
+    str x0, [sp, LSUMLENGTH]
+    // if (oSum->lLength <= lSumLength) goto endif2;
+    ldr x0, [sp, OSUM]
+    ldr x0, [x0]
+    ldr x1, [sp, LSUMLENGTH]
+    cmp x0, x1
+    ble endif2
+    // memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
+    ldr x0, [sp, OSUM]
+    // ldr x0, [x0, AULDIGITS]
+    add x0, x0, AULDIGITS
+    // assuming that 0 is treated as a long
+    mov x1, 0 
+    mov x2, MAX_DIGITS
+    lsl x2, x2, 3
+    bl memset
+    endif2:
+    // ulCarry = 0;
+    mov x0, 0
+    str x0, [sp, ULCARRY]
+    // lIndex = 0;
+    str x0, [sp, LINDEX]
+    loop1:
+    //if (lIndex >= lSumLength) goto loop1End;
+    ldr x0, [sp, LINDEX]
+    ldr x1, [sp, LSUMLENGTH]
+    cmp x0, x1
+    bge loop1End
+    // ulSum = ulCarry;
+    ldr x0, [sp, ULCARRY]
+    str x0, [sp, ULSUM]
+    // ulCarry = 0;
+    mov x0, 0
+    str x0, [sp, ULCARRY]
+    // ulSum += oAddend1->aulDigits[lIndex];
+    ldr x0, [sp, OADDEND1]
+    add x0, x0, AULDIGITS
+    ldr x1, [sp, LINDEX]
+    lsl x1, x1, 3
+    add x0, x0, x1
+    ldr x0, [x0]
+    ldr x2, [sp, ULSUM]
+    add x2, x2, x0
+    str x2, [sp, ULSUM]
+    // if (ulSum >= oAddend1->aulDigits[lIndex]) goto endif3;
+    ldr x0, [sp, OADDEND1]
+    add x0, x0, AULDIGITS
+    ldr x1, [sp, LINDEX]
+    lsl x1, x1, 3
+    add x0, x0, x1
+    ldr x0, [x0]     // x0 is the value of oAddend1->aulDigits[lIndex]
+    ldr x2, [sp, ULSUM]
+    cmp x2, x0
+    bhs endif3
+
+    //ulCarry = 1;
+    mov x0, 1
+    str x0, [sp, ULCARRY]
+    endif3:
+    // ulSum += oAddend2->aulDigits[lIndex];
+    ldr x0, [sp, OADDEND2]
+    add x0, x0, AULDIGITS
+    ldr x1, [sp, LINDEX]
+    lsl x1, x1, 3
+    add x0, x0, x1
+    ldr x0, [x0]
+    ldr x2, [sp, ULSUM]
+    add x2, x2, x0
+    str x2, [sp, ULSUM]
+    // if (ulSum >= oAddend2->aulDigits[lIndex]) goto endif4;
+    ldr x0, [sp, OADDEND2]
+    add x0, x0, AULDIGITS
+    ldr x1, [sp, LINDEX]
+    lsl x1, x1, 3
+    add x0, x0, x1
+    ldr x0, [x0]      // x0 is the value of oAddend2->aulDigits[lIndex]
+    ldr x2, [sp, ULSUM]
+    cmp x2, x0
+    bhs endif4
+
+    //  ulCarry = 1;
+    mov x0, 1
+    str x0, [sp, ULCARRY]
+
+    endif4:
+    // oSum->aulDigits[lIndex] = ulSum;
+    ldr x0, [sp, ULSUM]
+    ldr x1, [sp, OSUM]
+    add x1, x1, AULDIGITS
+    ldr x2, [sp, LINDEX]
+    lsl x2, x2, 3
+    add x1, x1, x2
+    str x0, [x1]
+    // lIndex++;
+    ldr x0, [sp, LINDEX]
+    add x0, x0, 1
+    str x0, [sp, LINDEX]
+    // goto loop1;
+    b loop1
+    loop1End:
+    // if (ulCarry != 1) goto endif5;
+    ldr x0, [sp, ULCARRY]
+    cmp x0, 1
+    bne endif5
+    // if (lSumLength != MAX_DIGITS) goto endif6;
+    ldr x0, [sp, LSUMLENGTH]
+    cmp x0, MAX_DIGITS
+    bne endif6
+
+    // return FALSE;
+
+    // Epilog and return lLarger
+        // the callee should save the return value in x0
+        mov     x0, FALSE
+        // ret branches to address x30
+        ldr     x30, [sp]
+        // pop the stack frame:
+        add     sp, sp, BIGINT_ADD_STACK_BYTECOUNT
+        ret
+        .size   BigInt_add, (. - BigInt_add)
+
+    endif6:
+// oSum->aulDigits[lSumLength] = 1;
+    mov x0, 1
+    ldr x1, [sp, OSUM]
+    add x1, x1, AULDIGITS
+    ldr x2, [sp, LSUMLENGTH]
+    lsl x2, x2, 3
+    add x1, x1, x2
+    str x0, [x1]
+    // lSumLength++;
+    ldr x0, [sp, LSUMLENGTH]
+    add x0, x0, 1
+    str x0, [sp, LSUMLENGTH]
+    endif5:
+
+    // oSum->lLength = lSumLength;
+    ldr x0, [sp, LSUMLENGTH]
+    ldr x1, [sp, OSUM]
+    str x0, [x1]
+// return TRUE;
+    // Epilog and return lLarger
+        // the callee should save the return value in x0
+        mov     x0, TRUE
+        // ret branches to address x30
+        ldr     x30, [sp]
+        // pop the stack frame:
+        add     sp, sp, BIGINT_ADD_STACK_BYTECOUNT
+        ret
+        .size   BigInt_add, (. - BigInt_add)
